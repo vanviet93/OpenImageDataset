28aa7dab3728154397da57cf0d2f21c119ce0472
vanviet93 <phamvanviet93@gmail.com>
2020-03-19 21:09:26 +0900

    frog, butterfly

--- segmentation/change_color.py
+++ segmentation/change_color.py
@@ -1,8 +1,9 @@
 import os
 import cv2
 import numpy as np
-source_folder = './image/turtle/'
-target_folder = './mask/turtle/'
+tag = 'butterfly1'
+source_folder = './image/' + tag + '/'
+target_folder = './mask/' + tag + '/'
 for file in os.listdir(source_folder):
 	img = np.float32(cv2.imread(source_folder + file))
 	img = img / 2
